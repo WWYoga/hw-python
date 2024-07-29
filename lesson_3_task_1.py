bd87beb77391f1f091f1cb45bce7598f315f59c5
WWYoga <144554692+WWYoga@users.noreply.github.com>
2024-07-29 08:47:13 +0300

    Add files via upload

---
+++ lesson_3_task_1.py
@@ -0,0 +1,6 @@
+name = input("enter the pet's nickname: ")
+age = input("enter the age of the pet: ")
+species = input("enter the type of pet: ")
+
+
+print(f"This is a {species} named {name}.Age: {age} years old.")
---
+++ lesson_3_task_2.py
@@ -0,0 +1,8 @@
+stage1 = input("enter the first stage of human development: ") #Australopithecus
+stage2 = input("enter the second stage of human development: ") #Homo habilis
+stage3 = input("enter the third stage of human development: ") #Homo erectus
+stage4 = input("enter the fourth stage of human development: ") #Homo neanderthalensis
+stage5 = input("enter the fifth stage of human development: ") #Homo sapiens
+stage6 = input("enter the sixth stage of human development: ")  #Homo sapiens sapiens
+
+print(stage1, stage2, stage3, stage4, stage5, stage6, sep=' => ')
